d5b3bf60b64bcf51665d709597e8c546aad0bb86
Darmatage <JasonWiserArt@gmail.com>
2024-11-07 20:45:41 -0500

    small fixes to web shooting scripts

--- cdan_fa24_action3/Assets/Scripts/Player_WebShooting.cs
+++ cdan_fa24_action3/Assets/Scripts/Player_WebShooting.cs
@@ -34,27 +34,27 @@ public class WebShooting : MonoBehaviour{
 
 	//for AIMING:
 	void FixedUpdate(){
-            //actual rotation uses vector math to calculate angle, then rotates character to mouse
-            Vector2 lookDir = mousePos - rb.position;
-            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg -90f;
-            //rb.rotation = angle;
-			webShooterBase.rotation=Quaternion.FromToRotation(Vector3.up, lookDir);
+    //actual rotation uses vector math to calculate angle, then rotates character to mouse
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg -90f;
+    //rb.rotation = angle;
+		webShooterBase.rotation=Quaternion.FromToRotation(Vector3.up, lookDir);
       }
 
 	//for SHOOTING:
 	public void WebShoot(){
-		//Get the location of the shooter, the distance to the mouseclick, and the midpoint of that distance
+	//Get the location of the shooter, the distance to the mouseclick, and the midpoint of that distance
 		Vector2 shootPoint = webShooter.position;
 		float webLength = Vector2.Distance(shootPoint, mousePos);
 		Vector2 midPoint = ((shootPoint - mousePos) * 0.5f) + mousePos;
 
-		//SPAWN the new web:
+	//SPAWN the new web:
 		GameObject newWeb = Instantiate(webPrefab, shootPoint, Quaternion.identity);
-		//SCALE: set length to distance between the two points (assumes starting length=1):
+	//SCALE: set length to distance between the two points (assumes starting length=1):
 		newWeb.transform.localScale = new Vector2(newWeb.transform.localScale.x, newWeb.transform.localScale.y * webLength);
-		//POSITION: center web at midpoint between two points
+	//POSITION: center web at midpoint between two points
 		newWeb.transform.position = midPoint;
-		//ROTATION: change orentation to the direction of the mouseclick from the player position 
+	//ROTATION: change orentation to the direction of the mouseclick from the player position 
 		Vector2 direction = mousePos - shootPoint;
         newWeb.transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
 	}
