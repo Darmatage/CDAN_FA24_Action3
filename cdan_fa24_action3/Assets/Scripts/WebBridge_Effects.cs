6cc8f283d0cf71d64fef851774ca6aec36d21217
Darmatage <JasonWiserArt@gmail.com>
2024-11-07 21:51:08 -0500

    more small fixes to web system, trying to make the collider more reliable

--- cdan_fa24_action3/Assets/Scripts/WebBridge_Effects.cs
+++ cdan_fa24_action3/Assets/Scripts/WebBridge_Effects.cs
@@ -4,27 +4,41 @@ using UnityEngine;
 
 public class WebBridge_Effects : MonoBehaviour{
 
+	public bool onWeb = false;
+
+/*
     public void OnTriggerEnter2D(Collider2D other){
         if (other.gameObject.tag=="Player"){
 			StartCoroutine(OnWeb(other.gameObject));
 		}
     }
-
-	public void OnTriggerExit2D(Collider2D other){
+*/
+	public void OnTriggerStay2D(Collider2D other){
         if (other.gameObject.tag=="Player"){
-			StartCoroutine(OffWeb(other.gameObject));
+			other.gameObject.GetComponent<Collider2D>().enabled=false;
+			Debug.Log("I turned off player collider");
+			onWeb = true;
 		}
     }
 
+	public void OnTriggerExit2D(Collider2D other){
+        if (other.gameObject.tag=="Player"){
+			//other.gameObject.GetComponent<Collider2D>().enabled=true;
+			//Debug.Log("player collider back on");
+			StartCoroutine(OffWeb(other.gameObject));
+			onWeb = false;
+		}
+    }
+/*
 	IEnumerator OnWeb(GameObject walker){
 		walker.GetComponent<Collider2D>().enabled=false;
 		Debug.Log("I turned off player collider");
 		yield return new WaitForSeconds(0.1f);
 	}
-
+*/
 
 	IEnumerator OffWeb(GameObject walker){
-		yield return new WaitForSeconds(0.1f);
+		yield return new WaitForSeconds(0.2f);
 		walker.GetComponent<Collider2D>().enabled=true;
 		Debug.Log("player collider back on");
 		yield return new WaitForSeconds(0.1f);
